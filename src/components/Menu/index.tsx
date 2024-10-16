e8cc4bef8d53efc02d9ab3d82750d29b9df13fe6
lutie0920 <lucianatie09@gmail.com>
2024-10-16 17:59:58 -0300

    feat: configurar o modal

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,6 +1,19 @@
-import { Card, Descricao, Titulo, Image } from './styles'
+import {
+  Card,
+  Descricao,
+  Titulo,
+  Image,
+  ImgClose,
+  ImgPizza,
+  ModalContent,
+  ModalStyle
+} from './styles'
+
+import marguerita from '../../assets/images/pizza.jpg'
+import close from '../../assets/images/close.png'
 
 import Button from '../Button'
+import { useState } from 'react'
 
 type Props = {
   name: string
@@ -8,15 +21,62 @@ type Props = {
   image: string
 }
 
-const Menu = ({ name, description, image }: Props) => (
-  <Card>
-    <Image src={image} />
-    <Titulo>{name}</Titulo>
-    <Descricao>{description}</Descricao>
-    <Button type="link" to="/" title="sobre o cardápio">
-      Adicionar ao carrinho
-    </Button>
-  </Card>
-)
+const Menu = ({ name, description, image }: Props) => {
+  const [modalEstaAberto, setModalEstaAberto] = useState(false)
+
+  return (
+    <>
+      <Card>
+        <Image src={image} />
+        <Titulo>{name}</Titulo>
+        <Descricao>{description}</Descricao>
+        <Button
+          type="button"
+          title="sobre o cardápio"
+          onClick={() => setModalEstaAberto(true)}
+        >
+          Adicionar ao carrinho
+        </Button>
+      </Card>
+      <ModalStyle className={modalEstaAberto ? 'visivel' : ''}>
+        <ModalContent className="container">
+          <ImgPizza src={marguerita} alt="imagem da pizza marguerita" />
+          <div>
+            <h4>Pizza Marguerita</h4>
+            <p>
+              A pizza Margherita é uma pizza clássica da culinária italiana,
+              reconhecida por sua simplicidade e sabor inigualável. Ela é feita
+              com uma base de massa fina e crocante, coberta com molho de tomate
+              fresco, queijo mussarela de alta qualidade, manjericão fresco e
+              azeite de oliva extra-virgem. A combinação de sabores é perfeita,
+              com o molho de tomate suculento e ligeiramente ácido, o queijo
+              derretido e cremoso e as folhas de manjericão frescas, que
+              adicionam um toque de sabor herbáceo. É uma pizza simples, mas
+              deliciosa, que agrada a todos os paladares e é uma ótima opção
+              para qualquer ocasião.
+            </p>
+            <p>Serve: de 2 a 3 pessoas</p>
+            <Button
+              type="link"
+              to="/Perfil"
+              title="clique aqui para adicionar ao carrinho"
+            >
+              Adicionar ao carrinho - R$ 60,90
+            </Button>
+          </div>
+          <ImgClose
+            src={close}
+            alt="fechar o modal"
+            onClick={() => setModalEstaAberto(false)}
+          />
+        </ModalContent>
+        <div
+          className="overlay"
+          onClick={() => setModalEstaAberto(false)}
+        ></div>
+      </ModalStyle>
+    </>
+  )
+}
 
 export default Menu
--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components'
 import { cores } from '../../styles'
-import { ButtonLink } from '../Button/styles'
+import { ButtonContainer, ButtonLink } from '../Button/styles'
 
 export const Card = styled.div`
   width: 320px;
@@ -8,7 +8,7 @@ export const Card = styled.div`
   background-color: ${cores.rosa};
   color: #ffebd9;
 
-  ${ButtonLink} {
+  ${ButtonContainer} {
     width: 304px;
     height: 24px;
     color: ${cores.rosa};
@@ -42,4 +42,78 @@ export const Descricao = styled.p`
   margin-right: 8px;
   margin-left: 8px;
   line-height: 22px;
+`
+
+export const ModalStyle = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: none;
+  align-items: center;
+  justify-content: center;
+
+  &.visivel {
+    display: flex;
+  }
+
+  .overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgb(0, 0, 0, 0.73);
+  }
+`
+
+export const ModalContent = styled.div`
+  display: flex;
+  padding-left: 32px;
+  padding-bottom: 32px;
+  position: relative;
+  z-index: 1;
+  background-color: ${cores.rosa};
+  color: ${cores.branco};
+
+  div {
+    margin-left: 24px;
+    margin-top: 32px;
+
+    h4 {
+      font-size: 18px;
+      font-weight: 900;
+    }
+
+    p {
+      width: 656px;
+      margin-top: 16px;
+      font-family: Roboto;
+      font-size: 14px;
+      font-weight: 400;
+      font-family: Roboto;
+      line-height: 22px;
+    }
+
+    ${ButtonLink} {
+      margin-top: 16px;
+      color: ${cores.rosa};
+      background-color: ${cores.bege};
+    }
+  }
+`
+
+export const ImgPizza = styled.img`
+  width: 280px;
+  height: 280px;
+  margin-top: 32px;
+  object-fit: cover;
+`
+
+export const ImgClose = styled.img`
+  width: 16px;
+  height: 16px;
+  margin-top: 8px;
+  margin-right: 8px;
 `
